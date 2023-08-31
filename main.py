2c05df787e8faa290a9aea571a2497b1419d2f2d
samrahasnain <143751042+samrahasnain@users.noreply.github.com>
2023-09-01 00:33:51 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,112 @@
+import argparse
+import os
+import torch
+import numpy
+import torch.nn as nn
+from torch.utils import data
+from dataset import get_loader
+from solver import Solver
+import time
+def main(config):
+    if config.mode == 'train':
+        train_loader = get_loader(config)
+
+        if not os.path.exists("%s/demo-%s" % (config.save_folder_rgb, time.strftime("%d"))):
+            os.mkdir("%s/demo-%s" % (config.save_folder_rgb, time.strftime("%d")))
+        config.save_folder_rgb = "%s/demo-%s" % (config.save_folder_rgb, time.strftime("%d"))
+        if not os.path.exists("%s/demo-%s" % (config.save_folder_depth, time.strftime("%d"))):
+            os.mkdir("%s/demo-%s" % (config.save_folder_depth, time.strftime("%d")))
+        config.save_folder_depth = "%s/demo-%s" % (config.save_folder_depth, time.strftime("%d"))
+        train = Solver(train_loader, None, config)
+        train.train()
+    elif config.mode == 'test':
+        get_test_info(config)
+        test_loader = get_loader(config, mode='test')
+        if not os.path.exists(config.test_folder_atts_rgb): os.makedirs(config.test_folder_atts_rgb)
+        if not os.path.exists(config.test_folder_dets_rgb): os.makedirs(config.test_folder_dets_rgb)
+        if not os.path.exists(config.test_folder_atts_depth): os.makedirs(config.test_folder_atts_depth)
+        if not os.path.exists(config.test_folder_dets_depth): os.makedirs(config.test_folder_dets_depth)
+        test = Solver(None, test_loader, config)
+        test.test()
+    else:
+        raise IOError("illegal input!!!")
+
+
+if __name__ == '__main__':
+    resnet101_path = '../pretrained-pytorch/resnet101-5d3b4d8f.pth'
+    resnet50_path = '../pretrained-pytorch/resnet50-19c8e357.pth'
+    vgg16_path = '../pretrained-pytorch/vgg16-397923af.pth'
+    conformer_path='../pretrained-pytorch/Conformer_base_patch16.pth'
+    cswin_path='../pretrained-pytorch/cswin_large_384.pth'
+    densenet161_path = '../pretrained-pytorch/densenet161-8d451a50.pth'
+    pretrained_path = {'resnet101': resnet101_path, 'resnet50': resnet50_path, 'vgg16': vgg16_path,
+                       'densenet161': densenet161_path,'conformer':conformer_path,'cswin':cswin_path}
+
+    parser = argparse.ArgumentParser()
+
+    # Hyper-parameters
+    parser.add_argument('--n_color', type=int, default=3)
+    parser.add_argument('--lr', type=float, default=0.0001)  # Learning rate resnet:4e-4
+    parser.add_argument('--wd', type=float, default=0.0005)  # Weight decay
+    parser.add_argument('--momentum', type=float, default=0.99)
+    parser.add_argument('--image_size', type=int, default=352)
+    parser.add_argument('--cuda', type=bool, default=True)
+    parser.add_argument('--device_id', type=str, default='cuda:0')
+    parser.add_argument('--qp_image_size', type=int, default=768)
+    parser.add_argument('--qp_batch_size', type=int, default=1)
+    parser.add_argument('--qp_pretrained_model', type=str, default='/kaggle/input/diqa-sip/final.pth')
+   
+    # Training settings
+    parser.add_argument('--arch', type=str, default='conformer'
+                        , choices=['resnet', 'vgg','densenet','conformer','cswin'])  # resnet, vgg or densenet
+    parser.add_argument('--pretrained_model', type=str, default=pretrained_path)  # pretrained backbone model
+    parser.add_argument('--epoch', type=int, default=120)
+    parser.add_argument('--batch_size', type=int, default=16)  # only support 1 now
+    
+    parser.add_argument('--num_thread', type=int, default=1)
+    parser.add_argument('--RGBload', type=str, default='')  # pretrained JL-DCF model
+    parser.add_argument('--Dload', type=str, default='')  # pretrained JL-DCF model
+    parser.add_argument('--save_folder_rgb', type=str, default='checkpoints_rgb/')
+    parser.add_argument('--save_folder_depth', type=str, default='checkpoints_depth/')
+    parser.add_argument('--epoch_save', type=int, default=5)
+    parser.add_argument('--iter_size', type=int, default=10)
+    parser.add_argument('--show_every', type=int, default=50)
+    parser.add_argument('--network', type=str, default='conformer'
+                        , choices=['resnet50', 'resnet101', 'vgg16', 'densenet161','conformer','cswin'])  # Network Architecture
+    #conformer setting
+    parser.add_argument('--patch_size', type=int, default=16)
+    parser.add_argument('--channel_ratio', type=int, default=6)
+    parser.add_argument('--embed_dim', type=int, default=576)
+    parser.add_argument('--depth', type=int, default=12)
+    parser.add_argument('--num_heads', type=int, default=9)
+    parser.add_argument('--mlp_ratio', type=int, default=4)
+    # Train data
+    parser.add_argument('--train_root', type=str, default='../RGBDcollection')
+    parser.add_argument('--train_list', type=str, default='../RGBDcollection/train.lst')
+    parser.add_argument('--img_folder', type=str, default='../DUTS-TR/DUTS-TR-Image')
+    parser.add_argument('--gt_folder', type=str, default='../DUTS-TR/DUTS-TR-Mask')
+    
+
+    # Testing settings
+    parser.add_argument('--RGBmodel', type=str, default='./checkpoints/demo-08/epoch_40.pth')  # Snapshot
+    parser.add_argument('--Dmodel', type=str, default='./checkpoints/demo-08/epoch_40.pth')  # Snapshot
+    parser.add_argument('--test_folder_atts_rgb', type=str, default='testint')  # Test results saving folder
+    parser.add_argument('--test_folder_dets_rgb', type=str, default='testint')  # Test results saving folder
+    parser.add_argument('--test_folder_atts_depth', type=str, default='testint')  # Test results saving folder
+    parser.add_argument('--test_folder_dets_depth', type=str, default='testint')  # Test results saving folder
+    parser.add_argument('--sal_mode', type=str, default='RGBD135',
+                        choices=['NJU2K', 'NLPR', 'STERE', 'RGBD135', 'LFSD', 'SIP', 'ReDWeb-S'])  # Test image dataset
+    parser.add_argument('--test_root', type=str, default='../testsod/RGBD135/RGBD135')
+    parser.add_argument('--test_list', type=str, default='../testsod/RGBD135/RGBD135/test.lst')
+    # Misc
+    parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'])
+    config = parser.parse_args()
+
+    if not os.path.exists(config.save_folder_rgb):
+        os.mkdir(config.save_folder_rgb)
+    if not os.path.exists(config.save_folder_depth):
+        os.mkdir(config.save_folder_depth)
+
+    #get_test_info(config)
+
+    main(config)
---
+++ solver.py
@@ -0,0 +1,141 @@
+import torch
+from torch.nn import functional as F
+from DCF_ResNet_models import DCF_ResNet
+import numpy as np
+import os
+import cv2
+import time
+import torch.nn as nn
+import argparse
+import os.path as osp
+import os
+from tqdm import trange, tqdm
+import torch
+class Solver(object):
+    def __init__(self, train_loader, test_loader, config):
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        self.config = config
+        self.iter_size = config.iter_size
+        self.show_every = config.show_every
+        #self.build_model()
+        self.RGBnet = DCF_ResNet()
+        self.Dnet = DCF_ResNet()
+       
+        if self.config.cuda:
+            self.RGBnet = self.RGBnet.cuda()
+            self.Dnet = self.Dnet.cuda()
+        self.lr = self.config.lr
+        self.wd = self.config.wd
+
+        self.RGBoptimizer = torch.optim.Adam(self.RGBnet.parameters(), lr=self.lr, weight_decay=self.wd)
+        self.Doptimizer = torch.optim.Adam(self.Dnet.parameters(), lr=self.lr, weight_decay=self.wd)
+        if config.mode == 'test':
+            print('Loading pre-trained model from %s...' % self.config.RGBmodel)
+            self.RGBnet.load_state_dict(torch.load(self.config.RGBmodel))
+            print('Loading pre-trained model from %s...' % self.config.Dmodel)
+            self.Dnet.load_state_dict(torch.load(self.config.Dmodel))
+        if config.mode == 'train':
+            if self.config.RGBload != '':
+                self.RGBnet.load_state_dict(torch.load(self.config.RGBload))  # load pretrained model
+                self.Dnet.load_state_dict(torch.load(self.config.Dload))  # load pretrained model
+    def test(self):
+        print('Testing...')
+        time_s = time.time()
+        img_num = len(self.test_loader)
+        for i, data_batch in enumerate(self.test_loader):
+            images, name, im_size, depth = data_batch['image'], data_batch['name'][0], np.asarray(data_batch['size']), \
+                                           data_batch['depth']
+            with torch.no_grad():
+                if self.config.cuda:
+                    device = torch.device(self.config.device_id)
+                    images = images.to(device)
+                    depth = depth.to(device)
+
+                atts_rgb, dets_rgb,x3_r,x4_r,x5_r = self.RGBnet(images)
+                atts_depth, dets_depth, x3_d,x4_d,x5_d = self.Dnet(depth)
+                atts_rgb = np.squeeze(torch.sigmoid(atts_rgbs)).cpu().data.numpy()
+                atts_rgb = (atts_rgb - atts_rgb.min()) / (atts_rgb.max() - atts_rgb.min() + 1e-8)
+                atts_rgb = 255 * atts_rgb
+                filename = os.path.join(self.config.test_folder_atts_rgb, name[:-4] + '_atts_rgb.png')
+                cv2.imwrite(filename, atts_rgb)
+                dets_rgb = np.squeeze(torch.sigmoid(dets_rgbs)).cpu().data.numpy()
+                dets_rgb = (dets_rgb - dets_rgb.min()) / (dets_rgb.max() - dets_rgb.min() + 1e-8)
+                dets_rgb = 255 * dets_rgb
+                filename = os.path.join(self.config.test_folder_dets_rgb, name[:-4] + '_dets_rgb.png')
+                cv2.imwrite(filename, dets_rgb)
+                atts_depth = np.squeeze(torch.sigmoid(atts_depths)).cpu().data.numpy()
+                atts_depth = (atts_depth - atts_depth.min()) / (atts_depth.max() - atts_depth.min() + 1e-8)
+                atts_depth = 255 * atts_depth
+                filename = os.path.join(self.config.test_folder_atts_depth, name[:-4] + '_atts_depth.png')
+                cv2.imwrite(filename, atts_depth)
+                dets_depth = np.squeeze(torch.sigmoid(dets_depths)).cpu().data.numpy()
+                dets_depth = (dets_depth - dets_depth.min()) / (dets_depth.max() - dets_depth.min() + 1e-8)
+                dets_depth = 255 * dets_depth
+                filename = os.path.join(self.config.test_folder_dets_depth, name[:-4] + '_dets_depth.png')
+                cv2.imwrite(filename, dets_depth)
+        time_e = time.time()
+        print('Speed: %f FPS' % (img_num / (time_e - time_s)))
+        print('Test Done!')
+    # training phase
+    def train(self):
+        iter_num = len(self.train_loader.dataset) // self.config.batch_size
+        step=0  
+        loss_vals=  []
+        self.RGBnet.train()
+        self.Dnet.train()
+        
+        for epoch in range(self.config.epoch):
+            loss_rgb_item = 0
+            loss_depth_item=0
+            for i, data_batch in tqdm(enumerate(self.train_loader)):
+                sal_image, sal_depth, sal_label= data_batch['sal_image'], data_batch['sal_depth'], data_batch[
+                        'sal_label']
+
+                
+                if (sal_image.size(2) != sal_label.size(2)) or (sal_image.size(3) != sal_label.size(3)):
+                    print('IMAGE ERROR, PASSING```')
+                    continue
+                if self.config.cuda:
+                    device = torch.device(self.config.device_id)
+                    sal_image, sal_depth, sal_label = sal_image.to(device), sal_depth.to(device), sal_label.to(device)
+                
+                step+=1
+                self.RGBoptimizer.zero_grad()
+                self.Doptimizer.zero_grad()
+                atts_rgb, dets_rgb,x3_r,x4_r,x5_r= self.RGBnet(sal_image)
+                atts_depth, dets_depth, x3_d,x4_d,x5_d= self.Dnet(sal_depth)
+                loss1_rgb = F.binary_cross_entropy_with_logits(atts_rgb, sal_label, reduction='sum')
+                loss2_rgb = F.binary_cross_entropy_with_logits(dets_rgb, sal_label, reduction='sum')
+                loss_rgb = (loss1_rgb + loss2_rgb) / 2.0
+                loss_rgb_item += loss_rgb.item() * sal_image.size(0)
+                loss_rgb.backward()
+                loss1_depth = F.binary_cross_entropy_with_logits(atts_depth, sal_label, reduction='sum')
+                loss2_depth = F.binary_cross_entropy_with_logits(dets_depth, sal_label, reduction='sum')
+                loss_depth = (loss1_depth + loss2_depth) / 2.0
+                loss_depth_item += loss_depth.item() * sal_depth.size(0)
+                loss_depth.backward()
+          
+                self.RGBoptimizer.step()
+                self.Doptimizer.step()
+
+
+            if (epoch + 1) % self.config.epoch_save == 0:
+                torch.save(self.RGBnet.state_dict(), '%s/DCF_rgb_epoch_%d.pth' % (self.config.save_folder_rgb, epoch + 1))
+                torch.save(self.Dnet.state_dict(), '%s/DCF_D_epoch_%d.pth' % (self.config.save_folder_depth, epoch + 1))
+            train_loss_rgb = loss_rgb_item/len(self.train_loader.dataset)
+            train_loss_depth = loss_depth_item/len(self.train_loader.dataset)
+          
+               
+            print('Epoch:[%2d/%2d] | Train Loss RGB: %.3f | Train Loss Depth : %0.3f' % (epoch, self.config.epoch,train_loss_rgb,train_loss_depth))
+                
+                
+        
+            # save model
+        torch.save(self.RGBnet.state_dict(), '%s/final.pth' % self.config.save_folder_rgb)
+        torch.save(self.Dnet.state_dict(), '%s/final.pth' % self.config.save_folder_depth)
+            
+    
+
+            
+        
